35b74e286f12787c345043bad07458c522c6b42a
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-02-17 12:07:50 +0600

    js part

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//  calculating part 
 document.getElementById('calculate-btn').addEventListener('click',function(){
     const incomeInput = document.getElementById('income-input')
     const incomeValue =parseFloat(incomeInput.value)
@@ -12,36 +12,55 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
     const clothesInput = document.getElementById('clothes-input')
     const clothesValue =parseFloat(clothesInput.value)
 
+    // calculate total expenses 
     const totalExpenses = foodValue + rentValue + clothesValue
 
     const expensesBalance =document.getElementById('total-expenses')
 
     const balance =document.getElementById('balance')
 
+    // set expenses 
     expensesBalance.innerText = totalExpenses
+    // set balance 
     balance.innerText = incomeValue - totalExpenses
 
-    incomeInput.value =''
     foodInput.value =''
     rentInput.value =''
     clothesInput.value =''
 })
 
+// saving part 
 document.getElementById('save-btn').addEventListener('click',function(){
     const saveInput =document.getElementById('save-input')
+    const income =document.getElementById('income-input')
+
+    const balance =document.getElementById('balance')
+
+    const savingAmount =document.getElementById('saving-amount')
+    const remaining = document.getElementById('remaining-balance')
+
     const saveValue = saveInput.value
 
+    const incomeValue = income.value
 
     const balanceValue =balance.innerText
 
-    const saving = (balanceValue * saveValue) / 100
+    // saving amount 
+    const saving = (incomeValue * saveValue) / 100
+  
 
-    const savingAmount =document.getElementById('saving-amount')
+ if(saving >= balanceValue){
+     alert("You can't save")
+ }
+ if(saveValue != 'number' || saveValue < 0){
+alert('Insert a valid number ⚠')
+ }else{
+    //  saving amount set 
     savingAmount.innerText = saving
 
-const remaining = document.getElementById('remaining-balance')
-
-remaining.innerText = balanceValue - saving
+    // remaining balance set 
+    remaining.innerText = balanceValue - saving
+ }
 
 saveInput.value =''
 })
